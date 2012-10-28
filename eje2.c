bfcc99056b06eff3982bb3f8d1d05788cd638b40
maxzo <sariego.isi@gmail.com>
2012-10-28 13:29:49 -0300

    Actualización eje2.c

--- eje2.c
+++ eje2.c
@@ -7,42 +7,75 @@
 int valor;
 
 void nodo1();
-/*void nodo1_1();
-void nodo1_2();*/
+void nodo1_1();
+void nodo1_2();
 void nodo2();
 
 void main(int argc, char* argv[])
 {
 	valor = atoi(argv[1]);
+	
+	printf("NODO R - VALOR = %d\nID PROCESO PRINCIPAL: %d\n", valor, getpid());
+	
 	int pid = fork();
 
 	if (pid == 0)
 	{
 		nodo1();
-		//nodo2();
 	}
 	else
 	{
-		printf("NODO R - VALOR = %d\nID PROCESO PRINCIPAL: %d\n", valor, pid);
+		wait(NULL);
+		pid = fork();
+		if (pid == 0)
+		{
+			nodo2();
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
 }
 
 void nodo1()
 {
-	int pid = fork();
-	int ppid = getppid();
+	valor += 100;
+	
+	printf("\nNODO 1 - VALOR = %d\nID NODO 1: %d - ID PADRE NODO 1 (NODO R): %d\n", valor, getpid(), getppid());
+	
+	int pidN1 = fork();
 
-	if (pid == 0)
+	if (pidN1 == 0)
 	{
-		
+		nodo1_1();
 	}
 	else
 	{
-		printf("\nNODO 1 - VALOR = %d\nID NODO 1: %d - ID PADRE NODO 1 (NODO R): %d\n", valor + 100, pid, ppid);
+		wait(NULL);
+		pidN1 = fork();
+		if (pidN1 == 0)
+		{
+			nodo1_2();
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
+}
+
+void nodo1_1()
+{
+	printf("\nNODO 1.1 - VALOR = %d\nID NODO 1.1: %d - ID padre NODO 1.1 (NODO 1): %d\n", valor * 2, getpid(), getppid());
+}
+
+void nodo1_2()
+{
+	printf("\nNODO 1.2 - VALOR = %d\nID NODO 1.2: %d - ID padre NODO 1.2 (NODO 1): %d\n", valor / 2, getpid(), getppid());
 }
 
 void nodo2()
 {
-	printf("\nNodo 1 - PID: %d\n", getpid());
+	printf("\nNODO 2 - VALOR = %d\nID NODO 2: %d - ID padre NODO 2 (NODO R): %d\n", valor - 100, getpid(), getppid());
 }
