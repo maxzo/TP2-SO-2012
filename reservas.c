6b8fc5e133b2a81903cb39e68fd7a2eca5404afd
gmichaut <gmichaut@gmail.com>
2012-10-29 18:59:21 -0500

    ejercicio 6

--- reservas.c
+++ reservas.c
@@ -10,10 +10,7 @@ void main(int argc, char* argv[])
 {
 	int fd;
 	int fw;
-	int vuelo = system("sed 's/$/_AR234/' pasajeros.txt");
-	int pipe;
-	char id_vuelo[6] = "_AR234";
-	/* char vuelo[] = "sed 's/$/_AR234/' pasajeros.txt"; */
+	int vuelo = system("sed 's/$/_AR234/' pasajeros.txt> temp.txt");
+	/* char id_vuelo[6] = "_AR234"; */
 	fd = open( "pasajeros.txt", O_RDWR | O_APPEND);
-	fw = write(fd, id_vuelo,6);
 }
