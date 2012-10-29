3a98ec523fd983dd961be12faf6bb86dd303019c
maxzo <sariego.isi@gmail.com>
2012-10-29 01:39:22 -0300

    Actualización tostadora.c

--- huesped.c
+++ huesped.c
@@ -4,19 +4,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void funcion(int sig) { }
+
 int main(int argc, char* argv[])
 {
-	int huesped = 1, i, j, pid = atoi(argv[1]);
+	signal(SIGUSR1, &funcion);
 		
+	int huesped = 1, i, j, pid = atoi(argv[1]);
+	
 	for (i = 0; i < 6; i++)
 	{
 		for (j = 0; j < 4; j++)
 		{
-			kill(pid, SIGUSR1);
 			printf("Pedido de tostada de Pan Blanco. Huésped: %d\n", huesped++);
+			kill(pid, SIGUSR1);
+			pause();
 		}
-		kill(pid, SIGUSR2);
 		printf("Pedido de tostada de Pan Negro. Huésped: %d\n", huesped++);
+		kill(pid, SIGUSR2);
+		pause();
 	}
 	
 	return 0;
--- tostadora.c
+++ tostadora.c
@@ -4,34 +4,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void tostarBlanco(int sig);
-void tostarNegro(int sig);
+void tostarBlanco(int sig, siginfo_t *siginfo, void *contexto);
+void tostarNegro(int sig, siginfo_t *siginfo, void *contexto);
 
 int tostadas = 0;
 
 int main()
 {
-	signal(SIGUSR1, &tostarBlanco);
-	signal(SIGUSR2, &tostarNegro);
+	struct sigaction accionB, accionN;
+	
+	accionB.sa_sigaction = &tostarBlanco;
+	accionN.sa_sigaction = &tostarNegro;
+	accionB.sa_flags = SA_SIGINFO;
+	accionN.sa_flags = SA_SIGINFO;
+	
+	sigaction(SIGUSR1, &accionB, NULL);
+	sigaction(SIGUSR2, &accionN, NULL);
 		
-	while (1)
-	{
-		pause();
-	}
+	while (1) pause();
 	
 	return 0;
 }
 
-void tostarBlanco(int sig)
+void tostarBlanco(int sig, siginfo_t *siginfo, void *contexto)
 {
 	printf("Deseo tostadas de pan blanco.\nTostadas de pan blanco. (%d)\n", ++tostadas);
-	//sleep(2);
+	sleep(2);
 	printf("Tostadas listas.\nTostadora libre, esperando pan.\n");
+	kill(siginfo->si_pid, SIGUSR1);
 }
 
-void tostarNegro(int sig)
+void tostarNegro(int sig, siginfo_t *siginfo, void *contexto)
 {
 	printf("Deseo tostadas de pan negro.\nTostadas de pan negro. (%d)\n", ++tostadas);
-	//sleep(1);
+	sleep(1);
 	printf("Tostadas listas.\nTostadora libre, esperando pan.\n");
+	kill(siginfo->si_pid, SIGUSR1);
 }
